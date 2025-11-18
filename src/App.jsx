41aeee3c3734edcbd41c1f448a7a403831fc8c03
Murtaza Rangwala <murtazahatimr@icloud.com>
2025-11-19 10:03:38 +1100

    Add react skeleton.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Vision Paper Table</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+        <p>
+          Edit <code>src/App.jsx</code> to get started.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
